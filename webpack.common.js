a515ca9563dae37e945e565449e7c0b8a9f7cd12
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-09-29 18:01:47 +0400

    "Fixed minor error - 'ERROR in Conflict: Multiple assets emit different content to the same filename index.html '""

--- webpack.common.js
+++ webpack.common.js
@@ -6,12 +6,6 @@ module.exports = {
   entry: {
     app: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Production",
-      template: "./src/index.html",
-    }),
-  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
